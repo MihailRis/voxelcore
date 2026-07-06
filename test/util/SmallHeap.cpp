baeb7eab68115d553429b95fbd7621e6b1b20fe0
MihailRis <mexaruc@gmail.com>
2026-07-06 23:06:26 +0300

    update test

--- test/util/SmallHeap.cpp
+++ test/util/SmallHeap.cpp
@@ -8,15 +8,11 @@ TEST(SmallHeap, Allocation) {
     auto index = 0;
     auto size = 4;
 
-    std::cout << 11 << std::endl;
     SmallHeap<uint16_t, uint8_t> map;
-    std::cout << 22 << std::endl;
     auto ptr = map.allocate(index, size);
-    std::cout << 33 << std::endl;
+    std::cout << "ptr: " << ptr << " begin: " << map.begin().data() << std::endl;
     EXPECT_EQ(map.sizeOf(ptr), size);
-    std::cout << 44 << std::endl;
     EXPECT_EQ(ptr, map.find(index));
-    std::cout << 55 << std::endl;
 }
 
 TEST(SmallHeap, MultipleAllocation) {
